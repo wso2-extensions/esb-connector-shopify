e1e6098396b103a5cb72ed03f70ad98d848dc9a9
RKathees <kathees@wso2.com>
2016-06-30 09:48:42 +0530

    Update ShopifyConnectorIntegrationTest.java

--- src/test/java/org/wso2/carbon/connector/integration/test/shopify/ShopifyConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/shopify/ShopifyConnectorIntegrationTest.java
@@ -43,7 +43,7 @@ public class ShopifyConnectorIntegrationTest extends ConnectorIntegrationTestBas
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("shopify-connector-1.0.1");
+        init("shopify-connector-1.0.2-SNAPSHOT");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
